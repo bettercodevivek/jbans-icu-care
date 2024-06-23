ff804b6aa88e6db11f3ce01cad778020c10be975
bettercodevivek <viveksingh0520@gmail.com>
2024-06-23 14:28:52 +0530

    Updation in codebase

--- jbans/src/Pages/Page2.jsx
+++ jbans/src/Pages/Page2.jsx
@@ -17,7 +17,7 @@ export default function Page2() {
         {/* Image Container */}
         <div className="relative">
           <img
-            src="https://i.postimg.cc/RF1xj2P3/IMG-20240619-WA0008-removebg-preview.webp"
+            src="https://i.postimg.cc/bwpJD15b/IMG-20240623-WA0010-removebg-preview.webp"
             className="max-w-full md:max-w-md lg:max-w-lg rounded-2xl"
             alt="Product"
             style={{
@@ -31,7 +31,7 @@ export default function Page2() {
 
       <div className="relative">
           <img
-            src="https://i.postimg.cc/RF1xj2P3/IMG-20240619-WA0008-removebg-preview.webp"
+            src="https://i.postimg.cc/SxdKWJ0P/IMG-20240623-WA0012-removebg-preview.webp"
             className="max-w-full md:max-w-md lg:max-w-lg rounded-2xl"
             alt="Product"
             style={{
@@ -63,7 +63,7 @@ export default function Page2() {
         {/* Image Container */}
         <div className="relative">
           <img
-            src="https://i.postimg.cc/RF1xj2P3/IMG-20240619-WA0008-removebg-preview.webp"
+            src="https://i.postimg.cc/Pqq5KTT5/IMG-20240623-WA0013-removebg-preview.webp"
             className="max-w-full md:max-w-md lg:max-w-lg rounded-2xl"
             alt="Product"
             style={{
@@ -72,6 +72,29 @@ export default function Page2() {
           />
           <div className="absolute inset-0 rounded-2xl border-2 border-transparent" />
         </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row items-center justify-center md:justify-evenly gap-8 md:gap-12 lg:gap-16">
+        {/* Image Container */}
+        <div className="relative">
+          <img
+            src="https://i.postimg.cc/FsjzHTTN/IMG-20240619-WA0007-removebg-preview.webp"
+            className="max-w-full md:max-w-md lg:max-w-lg rounded-2xl"
+            alt="Product"
+            style={{
+              filter: "drop-shadow(20px 20px 20px rgba(0, 0, 0, 0.25))",
+            }}
+          />
+          <div className="absolute inset-0 rounded-2xl border-2 border-transparent" />
+        </div>
+        <div className="bg-gradient-to-r from-red-500 via-red-500 to-red-700 bg-opacity-80 backdrop-blur-md shadow-2xl rounded-2xl p-8 max-w-lg md:max-w-md lg:max-w-lg flex flex-col transition-transform duration-500 ease-in-out transform hover:translate-y-1 hover:shadow-2xl hover:scale-105">
+          <h1 className="text-3xl font-bold text-white mb-6">Product Name</h1>
+          <p className="text-white leading-relaxed">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam atque eum eius corporis eaque sint odit reprehenderit debitis ex, unde porro praesentium dolorum nostrum cum laborum possimus consequatur voluptatum saepe
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam atque eum eius corporis eaque sint odit reprehenderit debitis ex, unde porro praesentium dolorum nostrum cum laborum possimus consequatur voluptatum saepe .
+          </p>
+        </div>
+        
       </div>
     </div>
   );
