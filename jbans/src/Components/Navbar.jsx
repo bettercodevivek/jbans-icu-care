9f1da6e9f77445fe3d5c5e0f6f4be759752dbe92
bettercodevivek <viveksingh0520@gmail.com>
2024-09-11 21:18:14 +0530

    Updation

--- jbans/src/Components/Navbar.jsx
+++ jbans/src/Components/Navbar.jsx
@@ -99,6 +99,12 @@ export default function Navbar() {
             <div className={`fixed inset-0 z-40 transition-opacity ${isSidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
                 <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-filter backdrop-blur-sm" onClick={toggleSidebar}></div>
                 <div className={`fixed inset-y-0 left-0 w-64 bg-white bg-opacity-70 backdrop-filter backdrop-blur-lg shadow-lg transform transition-transform duration-300 ease-in-out ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} rounded-r-xl`}>
+                <div className="flex justify-between items-center p-4">
+                <img
+                            src="https://i.postimg.cc/cJ2JxqMX/IMG-20240619-WA0009-1-removebg-preview.webp"
+                            className="h-16"
+                            alt="Sidebar Logo"
+                        />
                     <button
                         onClick={toggleSidebar}
                         type="button"
@@ -109,6 +115,7 @@ export default function Navbar() {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
                         </svg>
                     </button>
+                    </div>
                     <ul className="flex flex-col p-6 space-y-6">
                         <li>
                             <NavLink to='/home'
